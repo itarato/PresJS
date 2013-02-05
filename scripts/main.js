e91584de5d8bc402069fcac272657b859a1f7b4d
Peter Arato <it.arato@gmail.com>
2013-02-05 17:26:27 +0000

    Finish prototype

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,10 @@ requirejs.config({
 requirejs(['jquery', 'app/presjs', 'app/screening'], function($, PresJS, Screening) {
   console.log('Main file is loaded.');
 
+  $('#slide_theme').change(function(){
+    $('#slide_sample,#screen').attr('class', $('#slide_theme').val());
+  });
+
   $('#submit').click(function() {
     PresJS.start($('#mmxml').val());
     $('#screen,#pagers').show();
